efa4eddb4778e046015fa8472fd0b9d717d959a7
MarsCristian <cristian.martins.ccm@gmail.com>
2024-08-09 09:40:03 -0300

    Hello SpringBoot!

---
+++ src/main/java/br/ufscar/dc/dsw/SiteConsultas/SiteConsultasApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.SiteConsultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteConsultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteConsultasApplication.class, args);
+	}
+
+}
