49688d3fb6b946a356296c8054f9a9e40282c645
MarsCristian <cristian.martins.ccm@gmail.com>
2024-08-09 12:09:53 -0300

    Environment Ready
    Dockers and env ready to work

---
+++ src/main/java/br/ufscar/dc/dsw/SiteConsultas/controller/HomeController.java
@@ -0,0 +1,12 @@
+package br.ufscar.dc.dsw.SiteConsultas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
